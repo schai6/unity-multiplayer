6ea71052b29ab63c1e2fc72ffc523710c81bdec8
Sam Chai <sam.chai0501@gmail.com>
2018-01-19 11:28:02 -0500

    add health bars and decrease health when shot

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	void OnCollisionEnter()
+	void OnCollisionEnter(Collision collision)
 	{
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health>();
+		if (health != null)
+		{
+			health.TakeDamage(10);
+		}
 		Destroy(gameObject);
 	}
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	public RectTransform healthBar;
+	public const int maxHealth = 100;
+	public int currentHealth = maxHealth;
+
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+			Debug.Log("Dead!");
+		}
+
+		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+	}
+}
